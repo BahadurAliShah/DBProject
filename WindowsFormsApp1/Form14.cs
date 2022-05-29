5379bf99795fc4108803446901237e384fe8bac2
BahadurAliShah <96332131+BahadurAliShah@users.noreply.github.com>
2022-05-29 19:15:48 +0500

    Customer Purchase History Completed

---
+++ WindowsFormsApp1/Form14.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form14 : Form
+    {
+        public Form14()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("jdskkjds");
+            comboBox1.Items.Add("222222");
+        }
+
+        private void Form14_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
